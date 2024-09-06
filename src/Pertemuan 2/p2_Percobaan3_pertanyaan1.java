f0f060199d2c5984233aa3b556bb9eb1d82e2201
Najla-Nuricia <najlalaudy@gmail.com>
2024-09-06 17:20:29 +0700

    Pertanyaan 1

---
+++ src/Pertemuan 2/p2_Percobaan3_pertanyaan1.java
@@ -0,0 +1,9 @@
+public class p2_Percobaan3_pertanyaan1 {
+    public static void main(String[] args) {
+        System.out.println("KARTU RENCANA STUDI");
+        System.out.println("NIM           :244107020091");
+        System.out.println("NAMA          :NAJLA NURICIA LAUDY");
+        System.out.println("PROGRAM STUDI :D-IV TEKNIK INFORMATIKA");
+        System.out.println("KELAS         :1C");
+    }
+}
