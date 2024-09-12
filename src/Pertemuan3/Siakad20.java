2b973c8793f2aa3acbaa07f25fc4323e18de1a82
Najla-Nuricia <najlalaudy@gmail.com>
2024-09-12 14:04:10 +0700

    P3_Percobaan1_PertanyaanNo4

--- src/Pertemuan3/Siakad20.java
+++ src/Pertemuan3/Siakad20.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 public class Siakad20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String nama, nim;
-        char kelas;
+        String nama, nim, kelas;
         byte absen;
         double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
 
@@ -14,7 +13,7 @@ public class Siakad20 {
         System.out.print("Masukkan NIM: ");
         nim = sc.nextLine();
         System.out.print("Masukkan kelas: ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.print("Masukkan nomor absen: ");
         absen = sc.nextByte();
         System.out.print("Masukkan nilai kuis: ");
