603f8be9a9a40c72ff6da1f187f0a5c8277d4cd3
Najla-Nuricia <najlalaudy@gmail.com>
2024-09-13 18:50:40 +0700

    P3_TugasNo1

---
+++ src/Pertemuan3/P3_TugasNo1.java
@@ -0,0 +1,21 @@
+package Pertemuan3;
+import java.util.Scanner;
+
+public class P3_TugasNo1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    boolean lebihDari500kWH;
+    double tarifListrik, tagihanListrik;
+
+    System.out.print("Masukkan tarif listrik: ");
+    tarifListrik = input.nextDouble();
+    System.out.print("Apakah lebih dari 500 kWh (true/false): ");
+    lebihDari500kWH = input.nextBoolean();
+
+    tagihanListrik = (tarifListrik * 1500);
+
+    System.out.println("Total tagihan listrik Rp" + tagihanListrik);
+  }
+
+}
