c17b22d2553ced43e4db2e5bc53ef5c11ccde466
Najla-Nuricia <najlalaudy@gmail.com>
2024-09-26 06:57:49 +0700

    P5_Percobaan1

---
+++ src/Pertemuan5/PemilihanBilangan20.java
@@ -0,0 +1,21 @@
+package Pertemuan5;
+
+import java.util.Scanner;
+
+public class PemilihanBilangan20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan Sebuah Angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) 
+        {
+            System.out.println("Angka " + angka + " Termasuk bilangan genap");
+        } 
+        else {
+            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        }
+    }
+    
+}
