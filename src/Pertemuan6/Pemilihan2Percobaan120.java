8232a4eefecd9e044a38dfc96a9501ba971c65e6
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-03 12:01:30 +0700

    P6_Percobaan1

---
+++ src/Pertemuan6/Pemilihan2Percobaan120.java
@@ -0,0 +1,21 @@
+package Pertemuan6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan120 {
+    public static void main(String[] args) {
+        Scanner sc20 = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("masukkan tahun: ");
+        tahun = sc20.nextInt();
+
+      if ((tahun%4) ==0) {
+        if ((tahun%100) !=0) 
+            System.out.println("tahun kabisat");
+      } else  {
+        System.out.println("bukan tahun kabisat");
+      }
+    }
+}
