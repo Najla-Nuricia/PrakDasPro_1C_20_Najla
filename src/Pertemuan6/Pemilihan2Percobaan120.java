8a926e7d65017a8fb031015ff089cba8c7794655
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-03 12:10:58 +0700

    P6_Percobaan1_PertanyaanNo2

--- src/Pertemuan6/Pemilihan2Percobaan120.java
+++ src/Pertemuan6/Pemilihan2Percobaan120.java
@@ -11,11 +11,16 @@ public class Pemilihan2Percobaan120 {
         System.out.print("masukkan tahun: ");
         tahun = sc20.nextInt();
 
-      if ((tahun%4) ==0) {
-        if ((tahun%100) !=0) 
-            System.out.println("tahun kabisat");
-      } else  {
-        System.out.println("bukan tahun kabisat");
-      }
+        if ((tahun%4) ==0) {
+            if ((tahun%100) !=0) {
+               System.out.println("tahun kabisat"); 
+            } else if ((tahun%400) ==0) {
+                System.out.println("tahun kabisat");
+            } else {
+                System.out.println("bukan tahun kabisat");
+            }
+           } else {
+            System.out.println("bukan tahun kabisat");
+           }
     }
 }
