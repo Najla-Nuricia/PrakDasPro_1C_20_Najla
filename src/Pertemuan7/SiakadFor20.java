9178e3441f3f2f919682f0582a6e81a3f4ef0503
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-09 20:52:40 +0700

    P7_Percobaan1_Pertanyaan4

--- src/Pertemuan7/SiakadFor20.java
+++ src/Pertemuan7/SiakadFor20.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class SiakadFor20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
-        double nilai, tertinggi=100, terendah=0;
+        double nilai, tertinggi=0, terendah=100;
+
+        int lulus = 0, tidakLulus=0;
         
         for ( int i= 1; i<=10; i++) {
             System.out.print("masukkan nilai mahasiswa ke-" + i + ": ");
@@ -16,9 +18,16 @@ public class SiakadFor20 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >=60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("nilai tertinggi: " + tertinggi);
         System.out.println("nilai terendah: " + terendah);
+        System.out.println("Lulus: " + lulus);
+        System.out.println("tidak lulus: " + tidakLulus);
         sc.close();
     }
     
