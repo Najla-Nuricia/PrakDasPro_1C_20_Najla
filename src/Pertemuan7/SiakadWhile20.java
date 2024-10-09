0e5dbbc5e800683bf175ffa281965a23d39d989c
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-10 04:29:25 +0700

    P7_Percobaan2_PertanyaanNo4

--- src/Pertemuan7/SiakadWhile20.java
+++ src/Pertemuan7/SiakadWhile20.java
@@ -8,20 +8,20 @@ public class SiakadWhile20 {
 
         int nilai, jml, i=0;
 
-        System.out.println("masukkan jumlah mahasiswa: ");
+        System.out.print("masukkan jumlah mahasiswa: ");
         jml = sc.nextInt();
 
         while (i < jml) {
-            i++;
-            System.out.println("masukkan nilai mhs ke-" + (i + 1)+ ": ");
+            System.out.print("masukkan nilai mhs ke-" + (i + 1)+ ": ");
             nilai = sc.nextInt();
 
             if (nilai < 0 || nilai > 100) {
-                System.out.println("nilai tidak valid , masukkan nilai yang valid");
+                System.out.print("nilai tidak valid , masukkan nilai yang valid");
                 continue;
             }
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("nilai mhs ke-" + ( i + 1) + "adalah a"); 
+                System.out.println("bagus pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("nilai mhs ke-" + (i + 1)+ "adalah b+");
             } else if (nilai > 65 && nilai <= 73) {
@@ -35,7 +35,7 @@ public class SiakadWhile20 {
             } else {
                 System.out.println("nilai mhs ke-" + (i + 1)+ "adalah e");
             }
-    
+            i++;
             } 
             
         }
