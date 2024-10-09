2bfb28e8a98f2e1ce2943c57290db1b9fca5b82c
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-10 04:21:12 +0700

    P7_Percobaan2

---
+++ src/Pertemuan7/SiakadWhile20.java
@@ -0,0 +1,44 @@
+package Pertemuan7;
+
+import java.util.Scanner;
+
+public class SiakadWhile20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nilai, jml, i=0;
+
+        System.out.println("masukkan jumlah mahasiswa: ");
+        jml = sc.nextInt();
+
+        while (i < jml) {
+            System.out.println("masukkan nilai mhs ke-" + (i + 1)+ ": ");
+            nilai = sc.nextInt();
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("nilai tidak valid , masukkan nilai yang valid");
+                continue;
+            }
+            if (nilai > 80 && nilai <= 100) {
+                System.out.println("nilai mhs ke-" + ( i + 1) + "adalah a"); 
+            } else if (nilai > 73 && nilai <= 80) {
+                System.out.println("nilai mhs ke-" + (i + 1)+ "adalah b+");
+            } else if (nilai > 65 && nilai <= 73) {
+                System.out.println("nilai mhs ke-" + (i + 1)+ "adalah b");
+            } else if (nilai > 60 && nilai <= 65) {
+                System.out.println("nilai mhs ke-" + (i + 1)+ "adalah c+");
+            } else if (nilai > 50 && nilai <= 60) {
+                System.out.println("nilai mhs ke-" + (i + 1)+ "adalah c");
+            } else if (nilai > 39  && nilai <= 50) {
+                System.out.println("nilai mhs ke-" + (i + 1)+ "adalah d");
+            } else {
+                System.out.println("nilai mhs ke-" + (i + 1)+ "adalah e");
+            }
+            i++;
+    
+            } 
+            
+        }
+    }
+    
+
