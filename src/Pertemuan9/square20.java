9469d84e105ed0cebe1af309717bb0820311b209
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-24 18:06:58 +0700

    P9_Percobaan2_pertanyaanNo2

--- src/Pertemuan9/square20.java
+++ src/Pertemuan9/square20.java
@@ -6,8 +6,8 @@ public class square20 {
         System.out.print("masukkan nilai n: ");
         int n = sc20.nextInt();
 
-        for(int iOuter=0; iOuter<=n; iOuter++) {
-            for (int i = 1; i<=n; i++) {
+        for(int iOuter=1; iOuter<=n; iOuter++) {
+            for (int i = 0; i<=n; i++) {
                 System.out.print("*");
             }
             System.out.println();
