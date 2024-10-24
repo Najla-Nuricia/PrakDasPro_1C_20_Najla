1ad6e64ae68d0843d9e10c27de5d1093165bf239
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-24 13:00:29 +0700

    P9_Percobaan1_PertanyaanNo3

--- src/Pertemuan9/star20.java
+++ src/Pertemuan9/star20.java
@@ -7,7 +7,7 @@ public class star20 {
         System.out.print("masukkan nilai N: ");
         int N = sc20.nextInt();
 
-        for (int i=0;i>N;i++) {
+        for (int i=1;i<=N;i--) {
             System.out.print("*");
         }
         sc20.close();
