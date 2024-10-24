bba45b73a62f5d23e1e86bdd910873cac870db08
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-24 13:28:10 +0700

    P9_Percobaan2

---
+++ src/Pertemuan9/square20.java
@@ -0,0 +1,19 @@
+package Pertemuan9;
+import java.util.Scanner;
+public class square20 {
+    public static void main(String[] args) {
+        Scanner sc20 = new Scanner(System.in);
+        System.out.print("masukkan nilai n: ");
+        int n = sc20.nextInt();
+
+        for(int iOuter=1; iOuter<=n; iOuter++) {
+            for (int i = 1; i<=n; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc20.close();
+       
+    }
+    
+}
