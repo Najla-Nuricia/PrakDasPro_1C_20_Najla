b5fec405131c2765a2cdc6187e823f7aa1a468b7
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-31 13:41:12 +0700

    P10_Percobaan3_PertanyaanNo2

--- src/Pertemuan10/searchNilai20.java
+++ src/Pertemuan10/searchNilai20.java
@@ -1,21 +1,31 @@
 package Pertemuan10;
-
+import java.util.Scanner;
 public class searchNilai20 {
     public static void main(String[] args) {
-        int[] arrNilai = {80,85,78,98,90,82,86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
-        for (int i=0; i<arrNilai.length;i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
-                break;
+        int[] arrNilai = new int[10];
+        int hasil = 0;
+        int jmlMhs, nilaiDcr;
+
+        System.out.print("masukkan banyak nilai yang akan diinput: ");
+        jmlMhs = sc.nextInt();
+
+        for (int i=0; i<jmlMhs;i++) {
+            System.out.print("masukkan nilai mahasiswa ke-"+ (i+1) + " : ");
+            arrNilai[i] = sc.nextInt();
+            } 
+            System.out.print("masukkan nilai yg ingin dicari: ");
+            nilaiDcr = sc.nextInt();
+
+        for (int i = 0; i<jmlMhs;i++) {
+                if (nilaiDcr == arrNilai[i] ) {
+                    hasil = (i+1);
+                    System.out.println("nilai " + nilaiDcr + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+                } 
             }
+            sc.close();
         }
-        System.out.println();
-        System.err.println("nilai " + key + " ketemu di index ke-" + hasil);
-        System.out.println();
 
     }
     
-}
