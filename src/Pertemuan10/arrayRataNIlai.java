e14fff9b39f782b0dee3a06e39a364c28d386ad1
Najla-Nuricia <najlalaudy@gmail.com>
2024-10-31 11:20:34 +0700

    P10_Percobaan3_PertanyaanNo1

--- src/Pertemuan10/arrayRataNIlai.java
+++ src/Pertemuan10/arrayRataNIlai.java
@@ -7,16 +7,25 @@ public class arrayRataNIlai {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int lulus=0, tidakLulus = 0;
 
         for (int i=0; i< nilaiMhs.length; i++) {
             System.out.print("masukkan nilai mhs ke-"+(i+1)+" : ");
-            nilaiMhs[i] = sc.nextInt();
+            nilaiMhs[i] = sc.nextInt(); 
+            if (nilaiMhs[i] > 70 ) {
+               lulus++;
+            } else {
+                tidakLulus++;
+            }
         } 
         for (int i = 0; i < nilaiMhs.length;i++) {
             total += nilaiMhs[i];
-        }
+        } 
+        
         rata2 = total/nilaiMhs.length;
         System.out.println("rata - rata : "+ rata2);
+        System.out.println("mahasiswa yang lulus: " + lulus);
+        System.out.println("Mahasiaswa yang tidak lulus: " + tidakLulus);
 
         sc.close();;
 
