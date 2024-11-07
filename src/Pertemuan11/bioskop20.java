0053099b8ccc1f43eb57ba9c9de64b2ea883fa77
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-07 12:45:02 +0700

    P11_Percobaan1_PertanyaanNo7

--- src/Pertemuan11/bioskop20.java
+++ src/Pertemuan11/bioskop20.java
@@ -22,6 +22,11 @@ public class bioskop20 {
         for (String[] barisPenonton : penonton) {
             System.out.println("Panjang baris: " + barisPenonton.length);
         }
+
+        System.out.println("penonton baris ke-3");
+        for ( int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
         
     }
     
