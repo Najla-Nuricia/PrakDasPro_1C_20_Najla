013f4e0fcf47a2c67022a98a85ac8a1edf519254
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-08 12:47:45 +0700

    P11_Percobaan3

---
+++ src/Pertemuan11/numbers20.java
@@ -0,0 +1,11 @@
+package Pertemuan11;
+
+public class numbers20 {
+    public static void main(String[] args) {
+        int [][] myNumbers = new int [3][0];
+        myNumbers[0] = new int[5];
+        myNumbers[0] = new int[3];
+        myNumbers[0] = new int[1];
+    }
+    
+}
