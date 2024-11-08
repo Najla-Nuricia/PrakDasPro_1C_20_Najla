b4e0eebca7b7a7b0e7b44b392675bc249bea6dab
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-08 14:36:14 +0700

    P11_Percobaan4_PertanyaanNo1

--- src/Pertemuan11/SIAKAD20.java
+++ src/Pertemuan11/SIAKAD20.java
@@ -4,7 +4,12 @@ public class SIAKAD20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [][] nilai = new int[4][3];
+        System.out.print("masukkan jumlah mahasiswa: ");
+        int mhs = sc.nextInt();
+        System.out.print("masukkan jumlah mata kuliah: ");
+        int matkul = sc.nextInt();
+
+        int [][] nilai = new int[mhs][matkul];
 
         for (int i = 0; i< nilai.length;i++) {
             System.out.println("input nilai mahasiswa ke-" + (i+1));
@@ -16,20 +21,21 @@ public class SIAKAD20 {
                 totalPersiswa += nilai[i][j];
             }
 
-            System.out.println("nilai rata - rata: " + totalPersiswa/3);
+            System.out.println("nilai rata - rata: " + totalPersiswa/matkul);
         }
 
         System.out.println("\n===============================");
         System.out.println("Rata - rata Nilai setiap mata kuliah: "); 
 
-        for (int j = 0; j<3;j++) {
+        for (int j = 0; j< matkul;j++) {
             double totalPerMatkul = 0;
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < mhs; i++) {
                 totalPerMatkul += nilai[i][j];
             }
 
-            System.out.println("mata kuliah " + (j+1)+ ": " + totalPerMatkul/4);
+            System.out.println("mata kuliah " + (j+1)+ ": " + totalPerMatkul/mhs);
         }
+        sc.close();
     }
     
 }
