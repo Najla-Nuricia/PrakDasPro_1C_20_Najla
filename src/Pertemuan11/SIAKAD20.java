90b6885995334e307e28502efd102fe503ce8ee8
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-08 14:17:11 +0700

    P11_Percobaan4

--- src/Pertemuan11/SIAKAD20.java
+++ src/Pertemuan11/SIAKAD20.java
@@ -8,10 +8,27 @@ public class SIAKAD20 {
 
         for (int i = 0; i< nilai.length;i++) {
             System.out.println("input nilai mahasiswa ke-" + (i+1));
+            double totalPersiswa = 0;
+
             for (int j = 0; j<nilai[i].length;j++) {
                 System.out.print(" nilai mata kuliah "+ (j+1)+ ": ");
                 nilai[i][j] = sc.nextInt();
+                totalPersiswa += nilai[i][j];
             }
+
+            System.out.println("nilai rata - rata: " + totalPersiswa/3);
+        }
+
+        System.out.println("\n===============================");
+        System.out.println("Rata - rata Nilai setiap mata kuliah: "); 
+
+        for (int j = 0; j<3;j++) {
+            double totalPerMatkul = 0;
+            for (int i = 0; i < 4; i++) {
+                totalPerMatkul += nilai[i][j];
+            }
+
+            System.out.println("mata kuliah " + (j+1)+ ": " + totalPerMatkul/4);
         }
     }
     
