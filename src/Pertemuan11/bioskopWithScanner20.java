0fbfe10830fbbf948674e107a76736c5a0bf12d2
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-08 11:25:37 +0700

    P11_Percobaan2_pertanyaanNo2

--- src/Pertemuan11/bioskopWithScanner20.java
+++ src/Pertemuan11/bioskopWithScanner20.java
@@ -6,29 +6,55 @@ public class bioskopWithScanner20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String nama, next;
-        int baris, kolom;
+        String nama, next, data;
+        int baris, kolom, menu;
 
         String[][] penonton = new String[4][2];
 
         while (true) {
-            System.out.print("masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("masukkan kolom: ");
-            kolom = sc.nextInt();
+            System.out.println("===MENU===");
+            System.out.println("1. masukkan nama penonton");
+            System.out.println("2. tampilkan data ");
+            System.out.println("3. exit");
+            System.out.print("Masukkan opsi menu: ");
+            menu = sc.nextInt();
             sc.nextLine(); 
 
-            penonton[baris-1][kolom-1] = nama;
+            if (menu == 1) {
+                while (true) {
+                    System.out.print("masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan baris (1-4): ");
+                    baris = sc.nextInt();
+                    System.out.print("masukkan kolom (1-2): ");
+                    kolom = sc.nextInt();
+                    sc.nextLine(); 
 
-            System.out.print("input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                    penonton[baris - 1][kolom - 1] = nama;
 
-            if (next.equalsIgnoreCase("n")) {
+                    System.out.print("input penonton lainnya? (y/n): ");
+                    next = sc.nextLine();
+
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }
+            } else if (menu == 2) {
+            
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            System.out.print(penonton[i][j] + " ");
+                        }
+                        System.out.println();
+                }
+            } else if (menu == 3) {
+                System.out.println("Terima kasih!");
                 break;
+            } else {
+                System.out.println("Opsi tidak valid, silakan coba lagi.");
             }
         }
+
+        sc.close();
     }
-    
 }
