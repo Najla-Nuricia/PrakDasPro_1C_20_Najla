9d83934905001b7c3e2b4676b5d9191820b7648a
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-08 13:12:56 +0700

    P11_Percobaan3

--- src/Pertemuan11/numbers20.java
+++ src/Pertemuan11/numbers20.java
@@ -4,14 +4,12 @@ import java.util.Arrays;
 
 public class numbers20 {
     public static void main(String[] args) {
-        int [][] myNumbers = new int [3][0];
+        int [][] myNumbers = new int [3][];
         myNumbers[0] = new int[5];
-        myNumbers[0] = new int[3];
-        myNumbers[0] = new int[1];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
 
-        for (int i = 0; i<myNumbers.length;i++) {
-            System.out.println(Arrays.toString(myNumbers[i]));
-        }
+        
     }
     
 }
