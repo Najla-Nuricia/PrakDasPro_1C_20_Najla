c45be941abf4b6d035f9247971cd026d7350ff11
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-08 13:20:03 +0700

    P11_Percobaan3_PertanyaanNo1

--- src/Pertemuan11/numbers20.java
+++ src/Pertemuan11/numbers20.java
@@ -9,7 +9,11 @@ public class numbers20 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
 
-        
+        for (int i = 0; i<myNumbers.length;i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+
     }
     
 }
