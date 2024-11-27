78f18fb944107c93e9298d14f4b6905815bbb3d8
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-27 20:44:48 +0700

    P14_Percobaan4_PertanyaanNo2

--- src/Pertemuan14/PengunjungCafe19.java
+++ src/Pertemuan14/PengunjungCafe19.java
@@ -7,8 +7,8 @@ public class PengunjungCafe19 {
 
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("daftar nama pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String temp : namaPengunjung) {
+            System.out.println("- " + temp);
         }
     }
 }
