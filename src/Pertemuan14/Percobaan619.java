813663f520dfe501096c23afcd6310e583484b0e
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-27 21:14:30 +0700

    P14_Percobaan5_DenganFungsi

--- src/Pertemuan14/Percobaan619.java
+++ src/Pertemuan14/Percobaan619.java
@@ -3,7 +3,7 @@ package Pertemuan14;
 import java.util.Scanner;
 
 public class Percobaan619 {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int p , l , t, vol, Ls;
@@ -15,11 +15,19 @@ public class Percobaan619 {
         System.out.print("masukkan tinggi : ");
         t = sc.nextInt();
 
-        vol = p * l * t;
-        Ls = p * l;
-        System.out.println("luas persegi panjang " + Ls);
+        Ls = hitungLuas(p,l);
+        System.out.println("luas persegi panjang adalah " + Ls);
+        vol = hitungVolume(t, p, l);
         System.out.println("volume balok adalah " + vol);
-        sc.close();
+    }
+    static int hitungLuas (int p, int l) {
+        int Ls = p*l;
+        return Ls;
+    }
+
+    static int hitungVolume (int t, int pj , int lb) {
+        int volume = hitungLuas(pj, lb) * t;
+        return volume;
     }
     
 }
