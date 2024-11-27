8845a70e81814afc74190040bd0c151e39b78303
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-27 10:52:24 +0700

    P14_Percobaan3

--- src/Pertemuan14/Kafe19.java
+++ src/Pertemuan14/Kafe19.java
@@ -1,9 +1,25 @@
 package Pertemuan14;
-
+import java.util.Scanner;
 public class Kafe19 {
+
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Menu("andi" , true, "DISKON30");
+        Menu("andi" , false, "no");
+        System.out.print("\nmasukkan nomor menu yang ingin anda pesan : ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga19(pilihanMenu, banyakItem);
+        System.out.println("total harga untuk pesanan anda: Rp. " + totalHarga);
     }
+    public static int hitungTotalHarga19(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu-1]* banyakItem;
+        return hargaTotal;
+    }
+    
 
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("selamaat datang, " + namaPelanggan + " !");
@@ -16,7 +32,7 @@ public class Kafe19 {
             System.out.println("selamat anda mendapatkan diskon 50%");
         } else if (kodePromo.equals("DISKON30")) {
             System.out.println("selamat anda mendapatkan diskon 30%");
-        } else System.out.println("invalid");
+        } else System.out.println("invalid promo code!");
 
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. kopi hitam - Rp. 15.0000");
