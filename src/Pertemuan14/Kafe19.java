fa2f56653f598041e8c933389bd01905d2bf2f27
Najla-Nuricia <najlalaudy@gmail.com>
2024-11-27 09:39:15 +0700

    P14_Percobaan1

---
+++ src/Pertemuan14/Kafe19.java
@@ -0,0 +1,19 @@
+package Pertemuan14;
+
+public class Kafe19 {
+    public static void main(String[] args) {
+        menu();
+    }
+    public static void menu() {
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. kopi hitam - Rp. 15.0000");
+        System.out.println("2. cappuccino - Rp. 20.000");
+        System.out.println("3. latte - Rp. 22.000");
+        System.out.println("4. teh tarik - Rp. 12.000");
+        System.out.println("5. roti bakar - Rp. 10.000");
+        System.out.println("6. mie goreng - Rp. 18.000");
+        System.out.println("=========================");
+        System.out.println("silahkan pilih menu yang anda inginkan.");
+    }
+    
+}
